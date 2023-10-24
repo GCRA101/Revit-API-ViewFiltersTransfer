6dc3a55e89d31a566a992e4e2792c18b097e3bfb
GCRA101 <giorgio.albieri@burohappold.com>
2023-10-24 10:44:06 +0100

    Project StartUp

---
+++ ViewFiltersTransfer/Command.cs
@@ -0,0 +1,81 @@
+﻿/* IMPORT LIBRARIES */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Libraries for UI Buttons/Panels
+using System.Reflection;
+using System.Xaml;
+// Libraries for Revit
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB.Events;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+// Libraries for ETABS
+using ETABSv1;
+// Other Libraries
+using System.Linq.Expressions;
+using static System.Net.Mime.MediaTypeNames;
+using System.Windows.Forms;
+using System.Windows.Media;
+using System.Xml.Linq;
+using System.Runtime.CompilerServices;
+using Autodesk.Revit.Creation;
+using System.Security.Cryptography;
+using View = Autodesk.Revit.DB.View;
+
+namespace ViewFiltersTransfer
+{
+    /* COMMAND CLASS ************************************************************ */
+
+    [Transaction(TransactionMode.Manual)] 
+    public class Command : IExternalCommand
+    {
+        /*ATTRIBUTES*/
+        private Autodesk.Revit.UI.UIApplication uiApp;
+        private Autodesk.Revit.UI.UIDocument uiDoc;
+        private Autodesk.Revit.DB.Document doc;
+
+        /*IMPLEMENTED METHODS*/
+
+        // Execute Method from IExternalCommand Interface
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            try
+            {
+                //1. CREATE DB AND UI DOCUMENT OBJECTS
+                //Assignment
+                this.uiDoc = commandData.Application.ActiveUIDocument;
+                this.doc = uiDoc.Document;
+                //2. CALL COMMAND FUNCTION
+                transferViewFilters();
+                return Result.Succeeded;
+            }
+            catch (Exception e)
+            {
+                return Result.Failed;
+            }
+        }
+
+
+        /* METHODS */
+
+        //transferViewFilters()
+        private void transferViewFilters()
+        {
+            /*1. GET THE ACTIVE VIEW */
+            View activeView = doc.ActiveView;
+
+            /*2. GET ALL THE FILTERS' NAMES AND COLORS ASSIGNED TO THE ACTIVE VIEW */
+            Dictionary<String, Autodesk.Revit.DB.Color> filterColorsDict = activeView.GetFilters().
+                            ToDictionary(filterId => ((ParameterFilterElement)doc.GetElement(filterId)).Name.ToString(),
+                                         filterId=>activeView.GetFilterOverrides(filterId).SurfaceBackgroundPatternColor);
+
+
+
+        }
+
+    }
+}
---
+++ ViewFiltersTransfer/RibbonUI.cs
@@ -0,0 +1,78 @@
+﻿/* IMPORT LIBRARIES */
+using System;
+using System.Collections.Generic;
+// Libraries for Revit
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+using System.Linq;
+
+namespace ViewFiltersTransfer
+{
+    [Transaction(TransactionMode.Manual)]
+    public class RibbonUI : IExternalApplication
+    {
+        /* ATTRIBUTES */
+        //private String projectFolderPath = System.IO.Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).FullName;
+        private String projectFolderPath = "c:\\Users\\galbieri\\OneDrive - Buro Happold\\Structures\\10_Computational\\HOI\\Revit API Plugins\\ViewFiltersTransfer\\ViewFiltersTransfer\\";
+        private String imagesFolderPath, textFolderPath;
+        private RibbonPanel ribbonPanel;
+
+
+        /* CONSTRUCTOR */
+        //Default
+        public RibbonUI() { }
+
+
+        /* METHODS */
+
+        // OnStartup - Implemented from IExternalApplication Interface
+        public Result OnStartup(UIControlledApplication application)
+        {
+            try
+            {
+                //1. Get Ribbon Panel "View Filters"
+                RibbonPanel ribbonPanel = application.GetRibbonPanels("BH Plugins").
+                                          Find(rbPanel => rbPanel.Name == "View Filters");
+                               
+                //2. Buildup Inputs for RibbonItemFactory
+                imagesFolderPath = this.projectFolderPath + "Images\\";
+                textFolderPath = this.projectFolderPath + "Text Files\\";
+                String imagePath = imagesFolderPath + "AppLogo64x64.png";
+                String largeImagePath = imagesFolderPath + "AppLogo96x96.png";
+                String toolTipImagePath = imagesFolderPath + "AppLogo.png";
+                String toolTipText = "View Filters Transfer";
+                String longDescription = System.IO.File.ReadAllText(textFolderPath + "AppLongDescription.txt");
+                String assemblyFullPath = projectFolderPath + "bin\\Debug\\ViewFiltersTransfer.dll";
+                String className = "ViewFiltersTransfer.Command";
+
+                //3. Create RibbonItem (PushButton)
+                RibbonItemFactory.getInstance().create(ribbonPanel, RibbonItemType.PushButton, "View Filters\n Transfer", imagePath,
+                                                       largeImagePath, toolTipImagePath, toolTipText, longDescription, assemblyFullPath,
+                                                       className);
+                return Result.Succeeded;
+            }
+            catch (Exception e)
+            {
+                return Result.Failed;
+            }
+        }
+
+
+
+        // OnShutdown - Implemented from IExternalApplication Interface
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            try
+            {
+                return Result.Succeeded;
+            }
+            catch (Exception e)
+            {
+                return Result.Failed;
+            }
+        }
+    }
+
+
+}
